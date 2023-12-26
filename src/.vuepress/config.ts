af9db945e530ea6663205e94de35c507a571b4c8
Feny <1220301855@qq.com>
2023-12-26 17:13:08 +0800

    修改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/blogs",
+  base: "/",
 
   lang: "zh-CN",
   title: "Feny 博客",
