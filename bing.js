66bae7954361e03420d367f178162cdc68016c26
xFeny <33861286+xFeny@users.noreply.github.com>
2023-12-30 09:51:18 +0000

    Deploying to gh-pages from @ xFeny/xfeny.github.io@20bf44f09ada99debaf0e7d8004560a2d9a218cd 🚀

--- bing.js
+++
@@ -1,8 +0,0 @@
-// 必应每日壁纸API
-// https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN
-
-alert(100)
-const url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN';
-fetch(url).then(response => response.json())
-.then(data => console.log(data))
-.catch(e => console.log("Oops, error", e))
